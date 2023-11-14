b687e454ec7069d8a09bb3fc3a892f04072abea8
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-11-14 18:15:08 +0100

    Update template

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,56 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import fs from "fs";
+import fs, { readFile } from "fs/promises";
+import path from "path";
 import type { Plugin } from "rollup";
 import svgInlineLoader from "svg-inline-loader";
 import { defineConfig } from "vite";
 
-//TODO: remove this once https://github.com/vitejs/vite/pull/2909 gets merged
+/**
+ * @function svgLoader
+ * Responsible for inlining SVG images into the JS bundle
+ */
 export const svgLoader: () => Plugin = () => {
   return {
     name: "vite-svg-patch-plugin",
-    transform: function (code, id) {
+    transform: async function (code, id) {
       if (!id.endsWith(".svg")) return code;
 
       return `export default 'data:image/svg+xml;base64,${btoa(
-        svgInlineLoader.getExtractedSVG(fs.readFileSync(id, "utf8"))
+        svgInlineLoader.getExtractedSVG(
+          await fs.readFile(id, { encoding: "utf-8" })
+        )
       )}'`;
     },
   };
 };
-// https://vitejs.dev/config/
+
+export const appmod: () => Plugin = () => {
+  return {
+    name: "arcos-create-appmod",
+    async closeBundle() {
+      const dir = path.resolve(__dirname, "dist/assets");
+      const files = await fs.readdir(dir);
+
+      files.forEach(async (file: string) => {
+        const split = file.split(".");
+        const ext = split[split.length - 1];
+
+        if (ext != "js") return;
+
+        await fs.writeFile(
+          path.resolve(dir, "..", "app.base64"),
+          btoa(await readFile(path.resolve(dir, file), { encoding: "utf-8" }))
+        );
+
+        await fs.rename(
+          path.resolve(dir, file),
+          path.resolve(dir, `..`, `app.js`)
+        );
+      });
+    },
+  };
+};
+
 export default defineConfig({
   plugins: [
     svelte({
@@ -27,6 +60,7 @@ export default defineConfig({
       },
     }),
     svgLoader(),
+    appmod(),
   ],
   build: {
     assetsInlineLimit: 52428800,
